b7ce4cdfb92991d7f709979136467c1d37884a23
Lamk-S <kunlancelot@gmail.com>
2025-11-20 01:19:42 -0500

    Data Types

---
+++ Basic/03-DataTypes.py
@@ -0,0 +1,37 @@
+"""
+Tipo de datos Primitivos
+    -- Numericos
+        -- int (enteros)
+        -- float(decimales)
+    -- Cadenas
+        -- str (string)   
+    -- Booleanos
+        -- True/False   
+"""
+
+edad_cliente = 24           # int
+name_prod = "iPad"          # str
+fab_prod = 'Apple'          # str
+stock = 5                   # int
+precio_prod = 3999.90       # float
+isProduct = True            # bool
+
+                                                              
+""" 
+print(type(edad_cliente))
+print(type(name_prod))
+print(type(fab_prod))
+print(type(stock))
+print(type(precio_prod))
+print(type(isProduct))
+"""
+
+"""      
+indentacion
+"""
+
+if edad_cliente >=18:
+    print("Persona mayor de edad") #True
+    
+else:
+    print("Persona menor de edad") #False  
