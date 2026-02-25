d293bd4f45ee01cf536d65cbea03b78324c27f5f
Lamk-S <kunlancelot@gmail.com>
2026-02-24 20:58:56 -0500

    Sales website - Home view, routing and base template structure

--- Intermediate/09-Web-Development-Django/WDD02_Sales-website/core/views.py
+++ Intermediate/09-Web-Development-Django/WDD02_Sales-website/core/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Customer
 
-# Create your views here.
+class HomeView(ListView):
+    model = Customer
+    template_name = "index.html"
