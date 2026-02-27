ca6e32da427f7491bf876ba3325f8178fbd8b31f
Lamk-S <kunlancelot@gmail.com>
2026-02-27 00:00:12 -0500

    Sales System - Customer view

--- Intermediate/09-Web-Development-Django/WDD02_Sales-website/core/urls.py
+++ Intermediate/09-Web-Development-Django/WDD02_Sales-website/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, CustomerCreateView, CustomerDetailView
 
 app_name = "core"
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('customer/create', CustomerCreateView.as_view(), name='customer_create'),
+    path('customer/<int:pk>', CustomerDetailView.as_view(), name='customer_detail')
 ]
--- Intermediate/09-Web-Development-Django/WDD02_Sales-website/core/views.py
+++ Intermediate/09-Web-Development-Django/WDD02_Sales-website/core/views.py
@@ -1,6 +1,17 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Customer
 
 class HomeView(ListView):
     model = Customer
     template_name = "index.html"
+    
+class CustomerCreateView(CreateView):
+    model = Customer
+    template_name = 'customer_create.html'
+    fields = ['name', 'email', 'phone', 'address', 'state']
+    
+class CustomerDetailView(DetailView):
+    model = Customer
+    template_name = 'customer_detail.html'
