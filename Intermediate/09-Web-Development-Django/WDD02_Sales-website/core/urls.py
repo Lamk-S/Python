a516f2a368b4b2d0d104dff8df80b30f5539ac1e
Lamk-S <kunlancelot@gmail.com>
2026-02-28 18:54:10 -0500

    Sales system - Customer CRUD

--- Intermediate/09-Web-Development-Django/WDD02_Sales-website/core/urls.py
+++ Intermediate/09-Web-Development-Django/WDD02_Sales-website/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import HomeView, CustomerCreateView, CustomerDetailView
+from .views import HomeView, CustomerCreateView, CustomerDetailView, CustomerDeleteView, CustomerUpdateView
 
 app_name = "core"
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('customer/create', CustomerCreateView.as_view(), name='customer_create'),
-    path('customer/<int:pk>', CustomerDetailView.as_view(), name='customer_detail')
+    path('customer/detail/<int:pk>', CustomerDetailView.as_view(), name='customer_detail'),
+    path('customer/delete/<int:pk>', CustomerDeleteView.as_view(), name='customer_delete'),
+    path('customer/edit/<int:pk>', CustomerUpdateView.as_view(), name='customer_update')
 ]
--- Intermediate/09-Web-Development-Django/WDD02_Sales-website/core/views.py
+++ Intermediate/09-Web-Development-Django/WDD02_Sales-website/core/views.py
@@ -2,6 +2,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Customer
+from django.urls import reverse_lazy
 
 class HomeView(ListView):
     model = Customer
@@ -15,3 +16,18 @@ class CustomerCreateView(CreateView):
 class CustomerDetailView(DetailView):
     model = Customer
     template_name = 'customer_detail.html'
+    
+class CustomerDeleteView(DeleteView):
+    model = Customer
+    success_url = reverse_lazy('core:home')
+    template_name='confirm_customer_delete.html'
+    
+class CustomerUpdateView(UpdateView):
+    model = Customer
+    template_name = 'customer_create.html'
+    fields = ['name', 'email', 'phone', 'address', 'state']
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['edit'] = True
+        return context
