80c443f931dddce3890815a12a5f2cfa3235e696
Lamk-S <kunlancelot@gmail.com>
2026-04-04 09:05:44 -0500

    Add user registration view with form handling and validation

--- Intermediate/09-Web-Development-Django/WDD03_Security-website/accounts/views.py
+++ Intermediate/09-Web-Development-Django/WDD03_Security-website/accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
 
-# Create your views here.
+# Vista para registrar un nuevo usuario
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save() # Guarda el nuevo usuario en la base de datos
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Usuario dado de alta {username}")
+            return redirect('login') # Redirige a la página de login tras el registro
+    else:
+        form = UserRegisterForm()
+    
+    return render(request, 'accounts/register.html', {'form':form})
