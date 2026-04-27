d8d676b3622d61b8ffc82c00c58b85e6e84eed87
Lamk-S <kunlancelot@gmail.com>
2026-04-27 18:56:23 -0500

    feat(accounts): add LoginHistory model and signal to track user logins

--- Intermediate/09-Web-Development-Django/WDD03_Security-website/accounts/signals.py
+++ Intermediate/09-Web-Development-Django/WDD03_Security-website/accounts/signals.py
@@ -1,8 +1,11 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.contrib.auth.signals import user_logged_in
 
 from allauth.socialaccount.signals import social_account_added, social_account_updated
+
+from .models import Profile, LoginHistory # Importamos LoginHistory
 
 from .models import Profile
 
@@ -31,3 +34,24 @@ def populate_user_profile(request, sociallogin, **kwargs):
 
     user.save()
     profile.save()
+    
+# Historial de login
+@receiver(user_logged_in)
+def log_user_login(sender, request, user, **kwargs):
+    if request:
+        # Extraer la IP real del usuario
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        
+        # Extraer el User-Agent (Navegador y Sistema Operativo)
+        user_agent = request.META.get('HTTP_USER_AGENT', '')[:255]
+
+        # Guardar en la base de datos
+        LoginHistory.objects.create(
+            user=user,
+            ip_address=ip,
+            user_agent=user_agent
+        )
