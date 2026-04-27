7f0ca81261861f05503088569e347fcc3aeed759
Lamk-S <kunlancelot@gmail.com>
2026-04-27 18:56:46 -0500

    feat(accounts): create activity view and url routing for login history

--- Intermediate/09-Web-Development-Django/WDD03_Security-website/accounts/views.py
+++ Intermediate/09-Web-Development-Django/WDD03_Security-website/accounts/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import LoginHistory
 
 @login_required
 def home_view(request):
     return render(request, 'account/home.html')
+
+@login_required
+def activity_view(request):
+    # Obtenemos los últimos 15 inicios de sesión del usuario actual
+    logins = LoginHistory.objects.filter(user=request.user)[:15]
+    return render(request, 'account/activity.html', {'logins': logins})
