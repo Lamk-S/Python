bed6eb69acde0de18f82adfe96eb5bff06169abf
Lamk-S <kunlancelot@gmail.com>
2026-05-13 23:25:31 -0500

    feat: create employee model and register in admin interface

---
+++ Intermediate/09-Web-Development-Django/WDD05_Email-sender/core/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Empleados(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
+    
+    def __str__(self):
+        return self.nombre
